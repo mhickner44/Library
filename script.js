92a4f9fc56f932d1b282aaabc022b98bf4aad559
mhickner44 <rmicu1903@gmail.com>
2023-03-25 12:14:55 -0400

    add and remove bookcards functioning

--- script.js
+++ script.js
@@ -1,32 +1,139 @@
-let myLibrary = [];
-
-//user input
+const grid = document.querySelector(".gridWrapper");
 let addButton = document.querySelector(".addBook");
 let formInput = document.querySelector("#bookInfo");
-let addCard = document.getElementById("showCard");
-let bookCard = document.querySelector(".bookCard");
 
-let bookGrid = document.querySelector(".gridWrapper");
+class book {
+  constructor(title, author, pages, haveRead) {
+    this._title = title;
+    this._author = author;
+    this._pages = pages;
+    this._haveRead = haveRead;
+  }
 
-let currentBook = 0;
+  //need getters and setters to be able to use the book info
+  get getTitle() {
+    return this._title;
+  }
+  get getAuthor() {
+    return this._author;
+  }
+  get getPages() {
+    return this._pages;
+  }
+  get getHaveRead() {
+    return this._haveRead;
+  }
 
-function Book(title, author, pages, haveRead) {
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.haveRead = haveRead);
+  set setTitle(bookTitle) {
+    return (this._title = bookTitle);
+  }
+  set setAuthor(bookAuthor) {
+    return (this._author = bookAuthor);
+  }
+  set setPages(numPages) {
+    return (this._pages = numPages);
+  }
+  set setHaveRead(read) {
+    return (this._haveRead = read);
+  }
+
+  // set
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class library {
+  constructor() {
+    this.library = [];
+  }
+
+  addBook(title, author, pages, haveRead) {
+    let newBook = new book(title, author, pages, haveRead);
+    this.library.push(newBook);
+    return newBook;
+  }
+
+  removeBook(bookPlacement) {
+    this.library.splice(bookPlacement, 1);
+  }
+
+  get getLibrary() {
+    return this.library;
+  }
+  //probably need to have something that returns a books spot in the array
 }
 
-//display the form
+//bookcard that can be put on the dom
+
+class bookCard {
+  constructor() {
+    this.cardElement = document.createElement("div");
+    let del = document.createElement("div");
+
+    this.cardElement.classList.add("bookCard");
+    del.classList.add("exitButton");
+    del.innerText = "x";
+
+    this.cardElement.append(del);
+
+    this.cardElement.addEventListener("click", removeBook);
+  }
+
+  //fill the card with the specific book from the array
+  fillCard(cardNum, lib) {
+    //get the book from the library and then get the info fromt he book
+    let currentBook = lib.getLibrary[cardNum];
+
+    let bookInfo = [
+      `title:${currentBook.getTitle}`,
+      `By: ${currentBook.getAuthor}`,
+      `# of pages: ${currentBook.getPages}`,
+      `Read?: ${currentBook.getHaveRead}`,
+    ];
+
+    let nodes = bookInfo.map((book) => {
+      let div = document.createElement("div");
+      div.textContent = book;
+      return div;
+    });
+    //need to get
+    this.cardElement.append(...nodes);
+    // cardElement.append(del);
+  }
+  get getCard() {
+    return this.cardElement;
+  }
+}
+
+
+//display book card to gride in the display controller
+// lib.addBook("LOTR", "tolkein", 69, true);
+// lib.addBook("LOTR sequel", "RR martin", 69, true);
+// console.log(lib.getLibrary[1]);
+// console.log(lib);
+// let bookElement = new bookCard();
+// let bookElement2 = new bookCard();
+// bookElement.fillCard(0, lib);
+// bookElement2.fillCard(1, lib);
+// let card = bookElement.createCard();
+
+// grid.append(bookElement.getCard);
+// grid.append(bookElement2.getCard);
+// lib.removeBook(1);
+
+// removing the bookcard event listener original
+
+function removeBook(){
+//remove book card froim the dom 
+var index = Array.from(grid.children).indexOf(this);
+lib.removeBook(index);
+this.remove();
+}
+
+
+
 
 addButton.addEventListener("click", displayForm);
 
 function displayForm() {
-  
   let formStyles = window.getComputedStyle(formInput);
   let visibility = formStyles.getPropertyValue("display");
 
@@ -37,86 +144,34 @@ function displayForm() {
   }
 }
 
-//taking the form
 formInput.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  
   let bookTitle = formInput.elements["bookTitle"];
   let authorName = formInput.elements["authorName"];
   let pages = formInput.elements["pageNum"];
   let haveRead = formInput.elements["haveRead"];
- 
+  
   bookTitle = bookTitle.value;
   authorName = authorName.value;
   pages = pages.value;
   haveRead = haveRead.value;
 
-  let book1 = new Book(bookTitle, authorName, pages, haveRead);
+ lib.addBook(bookTitle, authorName, pages, haveRead);
+  let newBookCard =new bookCard();
+  // let book1 = new Book(bookTitle, authorName, pages, haveRead);
+//need the current number to fill it with 
+  //next in the lib to be filled 
+  newBookCard.fillCard(lib.getLibrary.length-1, lib);
 
-  addBookToLibrary(book1);
-  //pass in the current element
-  addToLib(myLibrary.length);
+  grid.append(newBookCard.getCard);
+
+  // //pass in the current element
+  // addToLib(myLibrary.length);
   e.target.reset();
   displayForm();
 });
 
-function addToLib(bookNum) {
- 
 
-  currentBook = myLibrary.length - 1;
 
-  const bookCard = document.createElement("div");
-  const del = document.createElement("div");
-
-  bookCard.classList.add("bookCard");
-  bookCard.id = bookNum;
-
-  //remove the dom element
-  bookCard.addEventListener("click", function (e) {
-    //gets its placenemt in the parent element nodelist
-    let selectedBook = e.target;
-    if (selectedBook.classList == "exitButton") {
-      removeBook(selectedBook.parentNode.id);
-      this.remove();
-    }
-  });
-
-  del.classList.add("exitButton");
-  del.innerText = "x";
-
-  let bookInfo = [
-    myLibrary[currentBook].title,
-    `By: ${myLibrary[currentBook].author}`,
-    `# of pages: ${myLibrary[currentBook].pages}`,
-    `Read?: ${readText(currentBook)}`,
-  ];
-
-  let nodes = bookInfo.map((book) => {
-    let div = document.createElement("div");
-    div.textContent = book;
-    return div;
-  });
-
-  bookCard.append(...nodes);
-  bookCard.append(del);
-
-  bookCard.children[0].id = "title";
-
-  bookGrid.appendChild(bookCard);
-}
-
-function removeBook(bookNum) {
-  bookNum--;
-  myLibrary.splice(bookNum, 1);
-}
-
-function readText(currentBook) {
-  let read;
-  if (myLibrary[currentBook].haveRead == "true") {
-    read = "Yes";
-  } else {
-    read = "No";
-  }
-  return read;
-}
+const lib = new library();
